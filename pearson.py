f715a5229819fef37e656708c53de6baadb25dc8
jorisheijkant <jorisheijkant@persgroep.net>
2024-12-03 09:38:37 +0100

    Added some calculations

--- pearson.py
+++ pearson.py
@@ -2,25 +2,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-data = pd.read_csv('data/gemeenten.csv', encoding='ISO-8859-1', sep=';')
+data = pd.read_csv('data/gemeenten_2.csv', encoding='ISO-8859-1', sep=';')
 
 # Some parsing from the raw sheet for python
 # Delete percentage signs and comma seperators to dots
-data['Alles elektrisch'] = data['Alles elektrisch'].str.replace('%', '')
+data['Elektrisch per inwoner'] = data['AANTAL VOERTUIGEN'].astype(int) / data['Inwoners'].replace(',', '.').astype(float)
 data['Stemmen op PVV'] = data['Stemmen op PVV'].str.replace('%', '').astype(float) / 100 
 data['Stemmen op coalitie'] = data['Stemmen op coalitie'].str.replace('%', '').astype(float) / 100 
-data['Alles elektrisch'] = data['Alles elektrisch'].str.replace(',', '.').astype(float)
+data['Elektrisch percentage (oud)'] = data['Alles elektrisch'].str.replace('%', '')
+data['Elektrisch percentage (oud)'] = data['Elektrisch percentage (oud)'].str.replace(',', '.').astype(float)
 data['Koopwoningen'] = data['Koopwoningen'].str.replace(',', '.').astype(float)
 
-# Check for missing or invalid data
-print(data.isnull().sum())
 data = data.dropna() 
 
-correlation = data[['Alles elektrisch', 'WOZ', 'Stemmen op coalitie', 'Bevolkingsdichtheid']].corr(method='pearson')
+correlation = data[['Elektrisch per inwoner', 'WOZ', 'Stemmen op coalitie', 'Bevolkingsdichtheid']].corr(method='pearson')
 print(correlation)
 
-sns.regplot(x='Bevolkingsdichtheid', y='Alles elektrisch', data=data, ci=None, line_kws={"color": "red"})
+data.to_csv("calculations.csv")
+
+sns.regplot(x='Bevolkingsdichtheid', y='Elektrisch per inwoner', data=data, ci=None, line_kws={"color": "red"})
 plt.title('Electric cars vs population density')
 plt.xlabel('Average population density (people per km2)')
-plt.ylabel('Percentage of electric cars')
+plt.ylabel('Electric cars per inhabitant')
 plt.show()
+
